51a1ac108c17305956554da79c756c4e71f5549d
asafeeson <g.kochergin@hotmail.com>
2025-05-14 00:10:17 +0200

    - add unplugin-icons module

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
 	plugins: [
@@ -11,6 +12,10 @@ export default defineConfig({
 		paraglideVitePlugin({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide'
+		}),
+		Icons({
+			compiler: 'svelte',
+			autoInstall: true
 		})
 	],
 	test: {
