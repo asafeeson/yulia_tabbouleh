011d68bb9800d8d6558b113824d8f3edc3539290
asafeeson <g.kochergin@hotmail.com>
2025-05-14 00:28:27 +0200

    refactor

---
+++ frontend/src/lib/components/shop/Footer.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	let { children }: { children: Snippet } = $props();
+</script>
+
+<div>
+	<h2>Это футер сайта</h2>
+	{@render children()}
+</div>
+
+<style>
+</style>
