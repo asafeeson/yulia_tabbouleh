b080d38c02ba65e6304aeadb21d3a80bb6e65727
asafeeson <g.kochergin@hotmail.com>
2025-05-14 02:03:04 +0200

    - add backgound image

--- frontend/src/lib/components/shop/CategoryBanner.svelte
+++ frontend/src/lib/components/shop/CategoryBanner.svelte
@@ -1,11 +1,39 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
+	import Image from './Image.svelte';
+	import Link from './Link.svelte';
 
-	let { children }: { children: Snippet } = $props();
+	interface Props {
+		linkUrl?: string;
+		backgroundImageSrc?: string;
+		children?: Snippet;
+		title?: string;
+		subtitle?: string;
+	}
+
+	let {
+		children,
+		backgroundImageSrc = 'https://picsum.photos/400',
+		linkUrl = '/',
+		title = 'Название категории товаров',
+		subtitle
+	}: Props = $props();
 </script>
 
-<div>
-	{@render children()}
+<div class="relative h-auto w-full overflow-clip">
+	<div class="w-3/4 space-y-6 p-10">
+		<h3>{title}</h3>
+		<p>{subtitle}</p>
+		{#if children}
+			{@render children()}
+		{/if}
+		<Link href={linkUrl}>Каталог</Link>
+	</div>
+	<div class="-z-1 absolute inset-0 bg-gradient-to-r from-amber-500 to-[transparent_50%]"></div>
+	<Image
+		src={backgroundImageSrc}
+		classImage="absolute right-0 top-1/2 -translate-y-1/2 object-cover h-full w-auto -z-1"
+	/>
 </div>
 
 <style>
