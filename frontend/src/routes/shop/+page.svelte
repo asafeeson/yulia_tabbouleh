6d7a964dbc7bb4f35005bc68dc800d67061fcc61
asafeeson <g.kochergin@hotmail.com>
2025-05-14 02:04:07 +0200

    - refactor

--- frontend/src/routes/shop/+page.svelte
+++ frontend/src/routes/shop/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
-    import type { PageData } from './$types';
+	import CategoryBanner from '$lib/components/shop/CategoryBanner.svelte';
+	import ShopMainPage from '$lib/components/shop/ShopMainPage.svelte';
+	import type { PageData } from './$types';
 
-    let { data }: { data: PageData } = $props();
+	let { data }: { data: PageData } = $props();
 </script>
+
+
+<ShopMainPage></ShopMainPage>
