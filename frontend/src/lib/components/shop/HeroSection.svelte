fcfa057cb51243cc031562ef41c3cafae70913ca
asafeeson <g.kochergin@hotmail.com>
2025-05-14 02:02:45 +0200

    - init

---
+++ frontend/src/lib/components/shop/HeroSection.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import Image from './Image.svelte';
+
+	let { children, backgroundImageSrc }: { children: Snippet, backgroundImageSrc: string } = $props();
+</script>
+
+<section class="flex items-center justify-center md:justify-start overflow-clip w-full bg-amber-200 relative h-auto">
+	{#if children}
+		{@render children()}
+	{/if}
+	<Image src={backgroundImageSrc} classImage="h-full w-auto object-cover"></Image>
+</section>
+
+<style>
+</style>
