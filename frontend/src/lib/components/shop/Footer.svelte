7019e388360f8f57c05d4a5eadbec59299090375
asafeeson <g.kochergin@hotmail.com>
2025-05-17 01:23:44 +0200

    fix: improve layout and styling of footer component for better responsiveness

--- frontend/src/lib/components/shop/Footer.svelte
+++ frontend/src/lib/components/shop/Footer.svelte
@@ -23,30 +23,32 @@
 	<div
 		class="mx-auto flex max-w-[1216px] flex-grow flex-col justify-between space-y-6 px-6 pb-4 pt-10 lg:px-0"
 	>
-		<div class="flex items-start justify-between px-6 lg:px-0">
-			<div class="flex-1/4 flex flex-col items-start justify-start gap-2">
+		<div class="flex flex-wrap items-center justify-between px-6 md:items-start lg:px-0">
+			<div
+				class="md:flex-1/4 mb-2 flex flex-col items-center w-full justify-start gap-2 lg:mb-0 lg:items-start"
+			>
 				<figure class="flex items-center justify-start">
 					<Logo className="h-10 w-auto" textColor="white"></Logo>
 				</figure>
 				<p>© Юля Таббуле, 2025</p>
 			</div>
-			<div class="flex-3/4 flex justify-around">
-				<nav>
-					<ul class="space-y-2">
+			<div class="lg:flex-3/4 flex items-center justify-center md:justify-around w-full flex-col md:flex-row md:space-y-0 space-y-2">
+				<nav class="w-full">
+					<ul class="space-y-2 text-center">
 						{#each menuData.menuOne as menu}
 							<li><a href={menu.url} itemprop="url" class="hover:text-white">{menu.name}</a></li>
 						{/each}
 					</ul>
 				</nav>
-				<nav>
-					<ul class="space-y-2">
+				<nav class="w-full">
+					<ul class="space-y-2 text-center">
 						{#each menuData.menuTwo as menu}
 							<li><a href={menu.url} itemprop="url" class="hover:text-white">{menu.name}</a></li>
 						{/each}
 					</ul>
 				</nav>
-				<nav>
-					<ul class="space-y-2">
+				<nav class="w-full">
+					<ul class="space-y-2 text-center">
 						{#each menuData.menuThree as menu}
 							<li><a href={menu.url} itemprop="url" class="hover:text-white">{menu.name}</a></li>
 						{/each}
@@ -57,8 +59,11 @@
 		<div>
 			<hr class="text-wolf-gray mb-2" />
 			<p class="text-center text-sm">
-				Сделано фулл-стек разработчиком&nbsp;<a href="https://asafeeson.dev" target="_blank" rel="noopener noreferrer" class="font-bold"
-					>asafeeson.dev</a
+				Сделано фулл-стек разработчиком&nbsp;<a
+					href="https://asafeeson.dev"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="font-bold">asafeeson.dev</a
 				>
 			</p>
 		</div>
