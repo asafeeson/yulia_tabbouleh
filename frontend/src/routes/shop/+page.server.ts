1d7378874e7cb297d3e3b41dfecbf26534b7d465
asafeeson <g.kochergin@hotmail.com>
2025-05-29 01:03:37 +0200

    - moving from medusajs to holy woocommerce

--- frontend/src/routes/shop/+page.server.ts
+++ frontend/src/routes/shop/+page.server.ts
@@ -1,10 +1,17 @@
 import type { PageServerLoad } from './$types';
-import { getMedusaClient } from '$lib/medusa';
+import woo from '$lib/woo/api';
 
 export const load: PageServerLoad = async (event) => {
-	const medusa = await getMedusaClient();
-	const { products } = await medusa.store.product.list({limit: 6, order: "-created_at"});
+	
+	const products = await woo.getProducts();
 	return {
 		productsBestSellers: products
 	};
 };
+// export const load: PageServerLoad = async (event) => {
+// 	const medusa = await getMedusaClient();
+// 	const { products } = await medusa.store.product.list({limit: 6, order: "-created_at"});
+// 	return {
+// 		productsBestSellers: products
+// 	};
+// };
