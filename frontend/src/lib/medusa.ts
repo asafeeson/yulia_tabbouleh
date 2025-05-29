2ee5f001c7e923df64ac6788ce9db77a843adb1d
asafeeson <g.kochergin@hotmail.com>
2025-05-30 01:15:22 +0200

    - comment out ifs

--- frontend/src/lib/medusa.ts
+++ frontend/src/lib/medusa.ts
@@ -2,13 +2,13 @@ import { MEDUSA_BACKEND_URL, NODE_ENV } from '$env/static/private';
 import { PUBLIC_MEDUSA_PUBLISHABLE_KEY } from '$env/static/public';
 import Medusa from '@medusajs/js-sdk';
 
-if (!MEDUSA_BACKEND_URL) {
-	throw Error('Нет URL для SSR доступа к Medusa бэкенду. Добавьте в .env MEDUSA_BACKEND_URL');
-}
+// if (!MEDUSA_BACKEND_URL) {
+// 	throw Error('Нет URL для SSR доступа к Medusa бэкенду. Добавьте в .env MEDUSA_BACKEND_URL');
+// }
 
-if (!PUBLIC_MEDUSA_PUBLISHABLE_KEY) {
-	throw Error('Нет ключа  доступа к бэкенду Medusa. Добавьте в .env MEDUSA_PUBLISHABLE_KEY');
-}
+// if (!PUBLIC_MEDUSA_PUBLISHABLE_KEY) {
+// 	throw Error('Нет ключа  доступа к бэкенду Medusa. Добавьте в .env MEDUSA_PUBLISHABLE_KEY');
+// }
 
 let sdkInstance: Medusa | null = null;
 
